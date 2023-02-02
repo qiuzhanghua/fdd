e39301ece94e2aaad5eb9e4e2c0c93e7911ed9f1
qiuzhanghua <qiuzhanghua@icloud.com>
2023-02-02 16:05:22 +0800

    inital

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learn Cucumber.io
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+val cucumberVersion: String by project
+
+dependencies {
+    testImplementation("io.cucumber:cucumber-java:${cucumberVersion}")
+    testImplementation("io.cucumber:cucumber-junit:${cucumberVersion}")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:${cucumberVersion}")
+    testImplementation("io.cucumber:cucumber-spring:${cucumberVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    implementation("com.google.guava:guava:31.1-jre")
+}
+
+application {
+    mainClass.set("com.example.App")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    systemProperty("cucumber.junit-platform.naming-strategy", "long")
+}
