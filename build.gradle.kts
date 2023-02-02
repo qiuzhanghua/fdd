cbb0d9b2fa5d37dec2e432e1ed2ba1211a8525a7
qiuzhanghua <qiuzhanghua@icloud.com>
2023-02-02 22:01:44 +0800

    Try feature

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     testImplementation("io.cucumber:cucumber-java:${cucumberVersion}")
     testImplementation("io.cucumber:cucumber-junit:${cucumberVersion}")
     testImplementation("io.cucumber:cucumber-junit-platform-engine:${cucumberVersion}")
-    testImplementation("io.cucumber:cucumber-spring:${cucumberVersion}")
+//    testImplementation("io.cucumber:cucumber-spring:${cucumberVersion}")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("com.google.guava:guava:31.1-jre")
 }
